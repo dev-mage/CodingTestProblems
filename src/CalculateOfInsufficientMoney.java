f2344ab1a146a757039585dd8cf327941af3b857
dev-mage <hxxlxx0@gmail.com>
2022-09-18 22:15:09 +0900

    2022-09-18) 프로그래머스 레벨1 - 부족한 금액 계산하기

---
+++ src/CalculateOfInsufficientMoney.java
@@ -0,0 +1,18 @@
+public class CalculateOfInsufficientMoney {
+    public long solution(int price, int money, int count) {
+        long answer = 0;
+        long total = 0;
+        for(int i = 1; i <= count; i++) {
+            total += (long) price * i;
+        }
+        if(total - money > 0) {
+            answer = total - money;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        CalculateOfInsufficientMoney cal = new CalculateOfInsufficientMoney();
+        System.out.println(cal.solution(3, 20, 4));
+    }
+}
