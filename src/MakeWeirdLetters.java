34f7c8584213ac247bab3f6120900a858f5af608
dev-mage <hxxlxx0@gmail.com>
2022-09-26 19:08:16 +0900

    2022-09-26 프로그래머스 레벨 1 이상한 문자 만들기

---
+++ src/MakeWeirdLetters.java
@@ -0,0 +1,36 @@
+public class MakeWeirdLetters {
+    public String solution(String s) {
+        String answer = "";
+        String[] strArr = s.split(" ", -1);
+        StringBuilder sb = new StringBuilder();
+
+//        if(strArr.length == 1 && strArr[0].equals("")) {
+//            return "";
+//        }
+        for(int i = 0; i < strArr.length; i++) {
+            String word = strArr[i];
+            if(word.equals("") && i != strArr.length - 1) {
+                sb.append(" ");
+            } else {
+                for(int j = 0; j < word.length(); j++) {
+                    char tempChar = word.charAt(j);
+                    if(j % 2 == 0) {
+                        sb.append(Character.toUpperCase(tempChar));
+                    } else {
+                        sb.append(Character.toLowerCase(tempChar));
+                    }
+                }
+                if(i != strArr.length - 1) {
+                    sb.append(" ");
+                }
+            }
+        }
+
+        answer = sb.toString();
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new MakeWeirdLetters().solution(" aBc   aBc   aBc "));
+    }
+}
